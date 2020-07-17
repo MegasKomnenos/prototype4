beb95b91f9fe15813a1b59418e76d0a7fde39b77
KJH <wogudwkd1233@gmail.com>
2020-07-17 13:27:45 +0900

    Rewrote the basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,281 +1,222 @@
+#![feature(get_mut_unchecked)]
+
+extern crate num_cpus;
+
 use legion::prelude::*;
 use legion::entity::Entity;
-use legion::storage::ComponentTypeId;
 use legion::systems::SystemBuilder;
 use legion::systems::resource::Resources;
 use legion::systems::schedule::Schedule;
 use rayon::ThreadPool;
 use rayon::ThreadPoolBuilder;
 
-use std::sync::mpsc::{ Sender, Receiver, channel };
+use std::sync::Arc;
+use std::sync::Barrier;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
+use std::sync::mpsc::Sender;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::channel;
 use std::collections::HashMap;
+use std::any::Any;
 
 enum LoopEvent {
-    Enable,
-    Disable,
     RemoveEntity(Entity),
-    RemoveComponent(Entity, ComponentTypeId),
-    ChangeComponent(Entity, ComponentTypeId, f32, fn(&mut World, &mut Resources, &Entity, &ComponentTypeId, f32)),
+    ChangeComponent(Entity, Wrapper<Box<dyn Any>>, fn(&mut World, &Entity, Box<dyn Any>)),
+    ChangeResource(Wrapper<Box<dyn Any>>, fn(&mut Resources, Box<dyn Any>)),
 }
 
-struct ScheduleWrapper {
-    schedule: Schedule,
+struct Wrapper<T> {
+    item: T,
 }
 
-unsafe impl Send for ScheduleWrapper {}
-
-#[derive(Clone)]
-struct SenderWrapper {
-    sender: Sender<LoopEvent>,
-}
-
-unsafe impl Send for SenderWrapper {}
-unsafe impl Sync for SenderWrapper {}
+unsafe impl<T> Send for Wrapper<T> {}
+unsafe impl<T> Sync for Wrapper<T> {}
 
 struct Loop {
-    name: String,
-    enabled: bool,
-    running: bool,
-    waiting: u8,
+    root: Arc<Barrier>,
     world: World,
     resources: Resources,
-    schedule: ScheduleWrapper,
-    receiver: Receiver<LoopEvent>,
+    schedule: Wrapper<Schedule>,
+    events: Wrapper<Receiver<LoopEvent>>,
+    on_schedule_start: Option<fn(&Resources, &Receiver<LoopEvent>)>,
+    on_schedule_end: Option<fn(&Resources, &Receiver<LoopEvent>)>,
 }
 
 impl Loop {
-    fn new(name: String, world: World, mut resources: Resources, schedule: Schedule) -> Self {
+    fn new(root: Arc<Barrier>, world: World, mut resources: Resources, schedule: Schedule) -> Self {
         let (producer, receiver) = channel::<LoopEvent>();
 
-        let sender = SenderWrapper { sender: producer };
-        let mut senders = HashMap::new();
-        senders.insert(name.clone(), sender.clone());
-
-        resources.insert(sender);
-        resources.insert(senders);
+        resources.insert(Wrapper { item: producer });
+        resources.insert(HashMap::<usize, Wrapper<Sender<LoopEvent>>>::new());
 
         Loop {
-            name,
-            enabled: false,
-            running: false,
-            waiting: 0,
+            root,
             world,
             resources,
-            schedule: ScheduleWrapper { schedule },
-            receiver,
+            schedule: Wrapper { item: schedule },
+            events: Wrapper { item: receiver },
+            on_schedule_start: None,
+            on_schedule_end: None,
         }
     }
 
-    fn execute(&mut self, pool: &mut ThreadPool) {
-        if !self.enabled || self.waiting >= 1 {
-            pool.install(|| {
-                self.handle_event();
-            });
-        } else if !self.running {
-            self.running = true;
+    fn start(&mut self, pool: &ThreadPool) {
+        pool.install(|| {
+            loop {
+                if let Some(func) = self.on_schedule_start {
+                    func(&self.resources, &self.events.item);
+                }
 
-            pool.install(|| {
-                self.schedule.schedule.execute(&mut self.world, &mut self.resources);
-                self.handle_event();
-                self.running = false;
-            });
-        }
-    }
-
-    fn subscribe(&mut self, lp: &Loop) {
-        let mut senders = self.resources.get_mut::<HashMap<String, SenderWrapper>>().unwrap();
-        let sender = lp.resources.get::<SenderWrapper>().unwrap();
-        senders.insert(self.name.clone(), sender.clone());
-    }
-    
-    fn unsubscribe(&mut self, lp: &Loop) {
-        let mut senders = self.resources.get_mut::<HashMap<String, SenderWrapper>>().unwrap();
-        senders.remove(&lp.name);
-    }
-
-    fn handle_event(&mut self) {
-        for event in self.receiver.try_iter() {
-            match event {
-                LoopEvent::Enable => self.enabled = true,
-                LoopEvent::Disable => self.enabled = false,
+                self.schedule.item.execute(&mut self.world, &mut self.resources);
                 
+                if let Some(func) = self.on_schedule_end {
+                    func(&self.resources, &self.events.item);
+                }
+            }
+        });
+
+        self.root.wait();
+    }
+
+    fn handle_events(&mut self) {
+        for event in self.events.item.try_iter() {
+            match event {
                 LoopEvent::RemoveEntity(entity) => {
                     self.world.delete(entity);
                 },
-                LoopEvent::RemoveComponent(entity, id) => {
-                    self.world.move_entity(entity, &[], &[id], &[], &[]);
-                }
-                LoopEvent::ChangeComponent(entity, id, val, func) => {
-                    func(&mut self.world, &mut self.resources, &entity, &id, val);
+                LoopEvent::ChangeComponent(entity, wrapper, func) => {
+                    func(&mut self.world, &entity, wrapper.item);
+                },
+                LoopEvent::ChangeResource(wrapper, func) => {
+                    func(&mut self.resources, wrapper.item);
                 }
             }
         }
+    }
+
+    fn set_on_schedule_start(&mut self, func: fn(&Resources, &Receiver<LoopEvent>)) {
+        self.on_schedule_start = Some(func);
+    }
+    fn set_on_schedule_end(&mut self, func: fn(&Resources, &Receiver<LoopEvent>)) {
+        self.on_schedule_end = Some(func);
     }
 }
 
 struct Link {
-    from: String,
-    to: String,
-    from_id: Option<usize>,
-    to_id: Option<usize>,
-    waiting: bool,
-    update: fn(&mut Vec<Loop>, usize, usize),
+    root: Arc<Barrier>,
+    from: Arc<Loop>,
+    to: Arc<Loop>,
+    barrier: Arc<Barrier>,
+    update: fn(Arc<Loop>, Arc<Loop>),
+    on_update_start: Option<fn(Arc<Loop>, Arc<Loop>)>,
+    on_update_end: Option<fn(Arc<Loop>, Arc<Loop>)>,
 }
 
 impl Link {
-    fn new(from: String, to: String, update: fn(&mut Vec<Loop>, usize, usize)) -> Self {
+    fn new(root: Arc<Barrier>, loops: &Vec<Arc<Loop>>, names: &Vec<String>, from: String, to: String, update: fn(Arc<Loop>, Arc<Loop>)) -> Self {
+        let mut from = loops[names.iter().position(|x| *x == from).unwrap()].clone();
+        let mut to = loops[names.iter().position(|x| *x == to).unwrap()].clone();
+        let barrier = Arc::new(Barrier::new(3));
+
+        unsafe {
+            Arc::get_mut_unchecked(&mut from).resources.insert(barrier.clone());
+            Arc::get_mut_unchecked(&mut to).resources.insert(barrier.clone());
+        }
+
         Link {
+            root,
             from,
             to,
-            from_id: None,
-            to_id: None,
-            waiting: false,
+            barrier,
             update,
-        }
-    }
-}
-
-struct LoopManager {
-    loops: Vec<Loop>,
-    pools: Vec<ThreadPool>,
-    links: Vec<Link>,
-    name_id: HashMap<String, usize>,
-}
-
-impl LoopManager {
-    fn new() -> Self {
-        LoopManager {
-            loops: Vec::new(),
-            pools: Vec::new(),
-            links: Vec::new(),
-            name_id: HashMap::new(),
+            on_update_start: None,
+            on_update_end: None,
         }
     }
 
-    fn add_loop(&mut self, lp: Loop, worker_num: usize) {
-        self.name_id.insert(lp.name.clone(), self.name_id.len());
-        self.loops.push(lp);
-        self.pools.push(ThreadPoolBuilder::new().num_threads(worker_num).build().unwrap());
-    }
+    fn start(&self, pool: &ThreadPool) {
+        pool.install(|| {
+            loop {
+                self.barrier.wait();
 
-    fn add_link(&mut self, mut link: Link) {
-        link.from_id = Some(self.name_id.get(&link.from).unwrap().clone());
-        link.to_id = Some(self.name_id.get(&link.to).unwrap().clone());
-        self.links.push(link);
-    }
+                if let Some(func) = self.on_update_start {
+                    func(self.from.clone(), self.to.clone());
+                }
 
-    fn execute(&mut self) {
-        loop {
-            for link in self.links.iter_mut() {
-                let from_id = link.from_id.unwrap();
-                let to_id = link.to_id.unwrap();
+                (self.update)(self.from.clone(), self.to.clone());
 
-                if self.loops[from_id].enabled && self.loops[to_id].enabled {
-                    if !link.waiting {
-                        if !self.loops[from_id].running {
-                            link.waiting = true;
-                            self.loops[from_id].waiting += 1;
-                        }
-                    }
-                    if link.waiting && !self.loops[to_id].running {
-                        link.waiting = false;
-                        self.loops[from_id].waiting -= 1;
-
-                        (link.update)(&mut self.loops, from_id, to_id);
-                    }
-                } else if link.waiting {
-                    link.waiting = false;
-                    self.loops[from_id].waiting -= 1;
+                if let Some(func) = self.on_update_end {
+                    func(self.from.clone(), self.to.clone());
                 }
             }
+        });
+    }
 
-            for (i, lp) in self.loops.iter_mut().enumerate() {
-                lp.execute(&mut self.pools[i]);
-            }
-        }
+    fn set_on_update_start(&mut self, func: fn(Arc<Loop>, Arc<Loop>)) {
+        self.on_update_start = Some(func);
+    }
+    fn set_on_update_end(&mut self, func: fn(Arc<Loop>, Arc<Loop>)) {
+        self.on_update_end = Some(func);
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-struct Test {
-    x: f32,
+struct Core {
+    root: Arc<Barrier>,
+    universe: Universe,
+    loops: Vec<Arc<Loop>>,
+    links: Vec<Link>,
+    pools: Vec<ThreadPool>,
+    pools_link: Vec<ThreadPool>,
+    names: Vec<String>,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-struct Tag {
-    id: usize,
-    other: Option<Entity>,
+impl Core {
+    fn new() -> Self {
+        let root = Arc::new(Barrier::new(4));
+        let universe = Universe::new();
+
+        Core {
+            root,
+            universe,
+            loops: Vec::new(),
+            links: Vec::new(),
+            pools: Vec::new(),
+            pools_link: Vec::new(),
+            names: Vec::new(),
+        }
+    }
+
+    fn load_application(&mut self) {
+        self.names.push("Application".to_string());
+        self.pools.push(ThreadPoolBuilder::new().num_threads(1).build().unwrap());
+        self.loops.push(Arc::new(Loop::new(self.root.clone(), self.universe.create_world(), Resources::default(), Schedule::builder().build())));
+    }
+
+    fn load_systems(&mut self) {
+        self.names.push("Systems".to_string());
+        self.pools.push(ThreadPoolBuilder::new().num_threads(num_cpus::get() - 1).build().unwrap());
+        self.loops.push(Arc::new(Loop::new(self.root.clone(), self.universe.create_world(), Resources::default(), Schedule::builder().build())));
+    }
+
+    fn load_link(&mut self) {
+        self.links.push(Link::new(self.root.clone(), &self.loops, &self.names, "Systems".to_string(), "Application".to_string(), |_, _| {}));
+        self.pools_link.push(ThreadPoolBuilder::new().num_threads(num_cpus::get()).build().unwrap());
+    }
+
+    fn run(&mut self) {
+        for (i, pool) in self.pools_link.iter().enumerate() {
+            self.links[i].start(pool);
+        }
+        for (i, pool) in self.pools.iter().enumerate() {
+            unsafe {
+                Arc::get_mut_unchecked(&mut self.loops[i]).start(pool);
+            }
+        }
+
+        self.root.wait();
+    }
 }
 
 fn main() {
-    let universe = Universe::new();
-
-    let mut world0 = universe.create_world();
-    let mut world1 = universe.create_world();
-
-    let num_of_countries = 100;
-
-    world0.insert(
-        (),
-        (0..num_of_countries).map(|i| (Test { x: 0. }, Tag { id: i, other: None }))
-    );
-    world1.insert(
-        (),
-        (0..num_of_countries).map(|i| (Test { x: 0. }, Tag { id: i, other: None }))
-    );
-
-    let query = <Write<Tag>>::query();
-
-    for (id_tag, mut tag) in query.iter_entities_mut(&mut world0) {
-        for (id_other, mut other) in query.iter_entities_mut(&mut world1) {
-            if tag.id == other.id {
-                tag.other = Some(id_other.clone());
-                other.other = Some(id_tag.clone());
-            }
-        }
-    }
-
-    let system0 = SystemBuilder::<()>::new("Test System 0")
-            .with_query(<Write<Test>>::query())
-            .build(move |_, world, _, queries| {
-                for mut test in queries.iter_mut(&mut *world) {
-                    test.x += 1.;
-                }
-            });
-    let system1 = SystemBuilder::<()>::new("Test System 1")
-            .with_query(<(Read<Test>, Read<Tag>)>::query())
-            .build(move |_, world, _, queries| {
-                for (test, tag) in queries.iter(world) {
-                    println!("{} {}", test.x, tag.id);
-                }
-            });
-
-    let schedule0 = Schedule::builder()
-            .add_system(system0)
-            .build();
-    let schedule1 = Schedule::builder()
-            .add_system(system1)
-            .build();
-
-    let loop0 = Loop::new("Test Loop 0".to_string(), world0, Default::default(), schedule0);
-    let loop1 = Loop::new("Test Loop 1".to_string(), world1, Default::default(), schedule1);
-
-    let link = Link::new("Test Loop 0".to_string(), "Test Loop 1".to_string(), |loops, from, to| {
-        let mut storage = HashMap::<Entity, f32>::new();
-
-        for (test, tag) in <(Read<Test>, Read<Tag>)>::query().iter(&loops[from].world) {
-            storage.insert(tag.other.unwrap(), test.x);
-        }
-        for (entity, mut test) in <Write<Test>>::query().iter_entities_mut(&mut loops[to].world) {
-            test.x = storage.get(&entity).unwrap() + 1.;
-        }
-    });
-
-    let mut app = LoopManager::new();
-
-    app.add_loop(loop0, 1);
-    app.add_loop(loop1, 1);
-    app.add_link(link);
-
-    app.execute();
 }
